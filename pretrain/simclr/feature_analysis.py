9f3537e9d23f889d03916cd72579a5f86e320f5d
YuliWanghust <44452178+YuliWanghust@users.noreply.github.com>
2024-08-07 15:09:08 -0400

    Add files via upload

---
+++ pretrain/simclr/feature_analysis.py
@@ -0,0 +1,99 @@
+from copy import deepcopy
+import numpy as np
+import os
+import pytorch_lightning as pl
+
+
+from args_parser import Arguments
+from dimensionality_reduction import perform_feature_analysis
+from dimensionality_reduction_clickview import perform_feature_analysis_view
+from downloader import Downloader
+from pretrain.simclr.utils import get_pretrained_model
+from utils import (
+    DIMENSIONALITY_REDUCTION_SAMPLES,
+    SEED,
+    SIMCLR_CHECKPOINT_PATH,
+    SplitType,
+    encode_data_features,
+    get_labels,
+    setup_device,
+)
+
+
+if __name__ == "__main__":
+    (
+        DATA_FLAG,
+        MODEL_NAME,
+        EXPLORE_TSNE_ONLY,
+        LEGEND,
+    ) = Arguments.parse_args_feature_analysis()
+
+    # Seed
+    pl.seed_everything(SEED)
+    np.random.seed(SEED)
+
+    # Setup device
+    device = setup_device()
+    print(f"Device: {device}")
+
+    # Load data
+    downloader = Downloader()
+    train_data = downloader.load(DATA_FLAG, SplitType.TRAIN)
+    test_data = downloader.load(
+        DATA_FLAG,
+        SplitType.TEST,
+        num_samples=DIMENSIONALITY_REDUCTION_SAMPLES,
+    )
+    
+    from torch.utils.data import Subset
+    # Creating a subset for example
+    subset_indices = [0, 1, 2, 3, 4]  # Just the first five entries
+    mnist_subset = Subset(test_data, subset_indices)
+    for i in mnist_subset.indices:
+        image, label = test_data[i]
+        print(image.shape, label)  # image is a tensor, label is the corresponding label of the image
+
+
+    train_labels = get_labels(train_data)
+    labels = get_labels(test_data)
+
+    # Load SimCLR model
+    encoder_path = os.path.join(SIMCLR_CHECKPOINT_PATH, MODEL_NAME)
+    encoder_model = get_pretrained_model(encoder_path)
+    print("SimCLR model loaded")
+
+    print("Preparing data features...")
+    network = deepcopy(encoder_model.convnet)
+    train_feats_data = encode_data_features(
+        network,
+        train_data,
+        device,
+        sort=False,
+    )
+    test_feats_data = encode_data_features(
+        network,
+        test_data,
+        device,
+        sort=False
+    )
+    print("Preparing data features: Done!")
+
+    perform_feature_analysis(
+        train_feats_data,
+        test_feats_data,
+        train_labels,
+        labels,
+        DATA_FLAG,
+        explore_tsne_only=EXPLORE_TSNE_ONLY,
+        legend=LEGEND,
+    )
+
+    # perform_feature_analysis_view(
+    #     train_feats_data,
+    #     test_feats_data,
+    #     labels,
+    #     test_data,
+    #     DATA_FLAG,
+    #     explore_tsne_only=EXPLORE_TSNE_ONLY,
+    #     legend=LEGEND,
+    # )
---
+++ pretrain/simclr/utils.py
@@ -0,0 +1,58 @@
+from copy import deepcopy
+import os
+
+from pretrain.simclr.simclrlm import SimCLRLM
+from pretrain.simsiam.simsiam import SimSiamLM
+from utils import encode_data_features
+
+
+def get_pretrained_model(path):
+    """
+    Load pretrained SimCLR model from a .ckpt checkpoint file.
+
+    Args:
+        path (str): The path to the checkpoint file.
+
+    Returns:
+        SimCLRLM: The loaded pretrained model.
+
+    Raises:
+        FileNotFoundError: If no file exists at the specified filepath.
+    """
+    # Check if pretrained model exists
+    if not os.path.isfile(path):
+        raise FileNotFoundError(
+            f"Pretrained model does not exist at: {path}"
+        )
+
+    return SimCLRLM.load_from_checkpoint(path)
+
+
+def get_data_features_from_pretrained_model(
+    pretrained_model,
+    dataset,
+    device,
+    batch_size=64,
+):
+    """
+    Given a pretrained model, extract the network encoder, remove the FC
+    layers, pass the dataset through the encoder and return the encoded features.
+
+    Args:
+        pretrained_model (SimCLRLM): The pretrained SimCLR model.
+        dataset (torch.utils.data.Dataset): The input dataset.
+        device (torch.device): Device used for computation.
+        batch_size (int, optional): The batch size. Defaults to 64.
+        sort (bool, optional): Sort the features by labels. Defaults to True.
+
+    Returns:
+        torch.utils.data.TensorDataset: Dataset containing the encoded
+            features and labels.
+    """
+    # Deep copy convolutional network
+    network = deepcopy(pretrained_model.convnet)
+    return encode_data_features(network, dataset, device, batch_size)
+
+
+def summarise():
+    print("Done! :)")
